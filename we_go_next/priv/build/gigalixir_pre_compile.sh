e1601480b65a282f3443fbcbb7fadab27c76c1e0
Ryan Messner <rpmessner@gmail.com>
2026-07-11 15:11:17 -0400

    Fix Zig setup for Gigalixir builds

--- we_go_next/priv/build/gigalixir_pre_compile.sh
+++ we_go_next/priv/build/gigalixir_pre_compile.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 set -eu
 
-mix zig.get
+zig_version="0.15.2"
+zig_root=".platform_tools/zig"
+zig_executable="${zig_root}/zig"
+
+if [ ! -x "${zig_executable}" ]; then
+  archive="/tmp/zig-${zig_version}.tar.xz"
+
+  curl -fsSL \
+    "https://ziglang.org/download/${zig_version}/zig-x86_64-linux-${zig_version}.tar.xz" \
+    -o "${archive}"
+  mkdir -p "${zig_root}"
+  tar -xJf "${archive}" -C "${zig_root}" --strip-components=1
+fi
+
+ln -sf "$(pwd)/${zig_executable}" .platform_tools/elixir/bin/zig
 mix assets.deploy
